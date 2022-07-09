02bf2d9f81317df2ceff5f6810599e890a9ea946
https-sam <osgo1115@icloud.com>
2022-07-09 11:54:00 -0600

    more options

--- src/components/Strategy/Algorithms/SelectionSort.js
+++ src/components/Strategy/Algorithms/SelectionSort.js
@@ -45,37 +45,25 @@ export default class SelectionSort  {
           var currentMinBar = parseInt(unSortedBars[minBarIndex].id);
           var currentNum = parseInt(unSortedBars[j].id);
 
-          // SG 07/07/2022 21:19  fins min value in the rest of the array
           
-          // Compare val1 & val2
-
+          // SG 07/07/2022 21:19  fins min value in the rest of the array          
           if (currentNum < currentMinBar) {
             if (minBarIndex !== i) {
-              
-
               unSortedBars[minBarIndex].style.backgroundColor = PROCESSING;
             }
             minBarIndex = j;
             unSortedBars[j].style.backgroundColor = CURRENT_MIN;
           } else {
-      
 
             unSortedBars[j].style.backgroundColor = PROCESSING;
           }
         }
 
-
         // SG 07/08/2022 13:09  SWAP Animation - swaps element id and height
         this.Animation.swap(i, minBarIndex);
     
-          
-        
-  
-        unSortedBars[i].style.backgroundColor = SORTAED;
-  
-    
+        unSortedBars[i].style.backgroundColor = SORTAED;  
     }
     console.log(unSortedBars);
-
   }
 }
