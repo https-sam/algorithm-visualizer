7a0a8ac401e928c7577f25e592aa72c9720cdba2
MicMetz <mime9599@colorado.edu>
2022-07-10 16:36:27 -0600

    finishing board rendering algo

--- src/components/Animation/Animation.js
+++ src/components/Animation/Animation.js
@@ -1,15 +1,17 @@
-import { Document } from "postcss";
+import {Document} from 'postcss';
+
+
 
 class Animation {
 
-  constructor() { 
+  constructor() {
     Animation.threads = [];
     // SG 07/09/2022 15:11  Singleton pattern
-    if(Animation._instance) {
+    if (Animation._instance) {
       return Animation._instance;
     }
     Animation._instance = this;
-    Animation.stop = false;
+    Animation.stop      = false;
   }
 
 
@@ -17,37 +19,39 @@ class Animation {
 
   static async clearTimeout() {
     Animation.stop = true;
-    for(const timeout of Animation.threads) {
+    for (const timeout of Animation.threads) {
       await this.clearTimeout(timeout);
     }
   }
 
+
   /**
    * takes indecies to be swapped
-   * @param {Integer} i 
+   * @param {Integer} i
    * @param {Integer} minBarIndex
    */
   swap(i, minBarIndex) {
-    let unSortedBars = document.querySelectorAll('.array-bars');
-    var temp1 = unSortedBars[minBarIndex].style.height;
+    let unSortedBars                       = document.querySelectorAll('.array-bars');
+    var temp1                              = unSortedBars[minBarIndex].style.height;
     unSortedBars[minBarIndex].style.height = unSortedBars[i].style.height;
-    unSortedBars[i].style.height = temp1;
+    unSortedBars[i].style.height           = temp1;
 
-    var temp2 = unSortedBars[minBarIndex].id;
+    var temp2                    = unSortedBars[minBarIndex].id;
     unSortedBars[minBarIndex].id = unSortedBars[i].id;
-    unSortedBars[i].id = temp1;
+    unSortedBars[i].id           = temp1;
   }
 
 
   static async getAnimation(delay) {
-    if(!Animation.stop) {
+    if (!Animation.stop) {
       await new Promise((resolve) =>
-        Animation.threads.push(setTimeout(() => {
-        resolve();
-      }, delay)));
+          Animation.threads.push(setTimeout(() => {
+            resolve();
+          }, delay)));
     }
   }
 }
 
 
-export { Animation }
+
+export {Animation};
--- src/components/Page/Container.js
+++ src/components/Page/Container.js
@@ -1,79 +1,79 @@
-import React, {Component}    from 'react';
-import {BuildTrigger}        from '../../modals/buttons/buildbutton/BuildTrigger';
-import {BuildForm}           from '../../modals/form/BuildForm';
-
-// TODO: To MainCanvas
-
-export class Container extends Component {
-	state = {isShown: false};
-
-	showModal = () => {
-		this.setState({isShown: true}, () => {
-			this.closeModal = this.closeModal.bind(this);
-			this.closeButton.focus();
-		});
-		this.toggleScrollLock();
-	};
-
-	onCloseModal = () => {
-		this.setState({isShown: false});
-		this.toggleScrollLock();
-
-	}
-
-	onClickOutside = (e) => {
-		if (this.modalRef && !this.modalRef.contains(e.target)) {
-			this.onCloseModal();
-		}
-	}
-
-	onKeyDown = (e) => {
-		if (e.key === 'Escape') {
-			this.onCloseModal();
-		}
-
-		else {
-			switch (e.key) {
-				case 'Enter': {
-
-					break;
-				}
-				case 'ArrowUp': {
-
-					break;
-				}
-				case 'ArrowDown': {
-
-					break;
-				}
-				default:
-					break;
-			}
-		}
-	}
-
-	toggleScrollLock = () => {
-		document.querySelector('html').classList.toggle('scroll-lock');
-	}
-
-
-	render() {
-		return (
-			<React.Fragment>
-				<BuildTrigger triggerID = "bar" buttonRef = {this.buttonRef} showModal = {this.showModal} onSubmit = {this.onSubmit}/>
-				{this.state.isShown ? (
-					<BuildForm
-						onSubmit = {this.onSubmit}
-						onCloseModal = {this.onCloseModal}
-						onClickOutside = {this.onClickOutside}
-						onKeyDown = {this.onKeyDown}
-					/>
-				) : null}
-			</React.Fragment>
-		)
-	}
-}
-
-
-
-export default Container;
+// import React, {Component}    from 'react';
+// import {BuildTrigger}        from '../../modals/buttons/buildbutton/BuildTrigger';
+// import {BuildForm}           from '../../modals/form/BuildForm';
+//
+// // TODO: To MainCanvas
+//
+// export class Container extends Component {
+// 	state = {isShown: false};
+//
+// 	showModal = () => {
+// 		this.setState({isShown: true}, () => {
+// 			this.closeModal = this.closeModal.bind(this);
+// 			this.closeButton.focus();
+// 		});
+// 		this.toggleScrollLock();
+// 	};
+//
+// 	onCloseModal = () => {
+// 		this.setState({isShown: false});
+// 		this.toggleScrollLock();
+//
+// 	}
+//
+// 	onClickOutside = (e) => {
+// 		if (this.modalRef && !this.modalRef.contains(e.target)) {
+// 			this.onCloseModal();
+// 		}
+// 	}
+//
+// 	onKeyDown = (e) => {
+// 		if (e.key === 'Escape') {
+// 			this.onCloseModal();
+// 		}
+//
+// 		else {
+// 			switch (e.key) {
+// 				case 'Enter': {
+//
+// 					break;
+// 				}
+// 				case 'ArrowUp': {
+//
+// 					break;
+// 				}
+// 				case 'ArrowDown': {
+//
+// 					break;
+// 				}
+// 				default:
+// 					break;
+// 			}
+// 		}
+// 	}
+//
+// 	toggleScrollLock = () => {
+// 		document.querySelector('html').classList.toggle('scroll-lock');
+// 	}
+//
+//
+// 	render() {
+// 		return (
+// 			<React.Fragment>
+// 				<BuildTrigger triggerID = "bar" buttonRef = {this.buttonRef} showModal = {this.showModal} onSubmit = {this.onSubmit}/>
+// 				{this.state.isShown ? (
+// 					<BuildForm
+// 						onSubmit = {this.onSubmit}
+// 						onCloseModal = {this.onCloseModal}
+// 						onClickOutside = {this.onClickOutside}
+// 						onKeyDown = {this.onKeyDown}
+// 					/>
+// 				) : null}
+// 			</React.Fragment>
+// 		)
+// 	}
+// }
+//
+//
+//
+// export default Container;
