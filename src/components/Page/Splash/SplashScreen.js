d7c4bb7f81b14beb57ff56ba079d89f158a9f712
MicMetz <mime9599@colorado.edu>
2022-07-09 18:47:30 -0600

    minor presentation changes

--- src/components/Page/Splash/SplashScreen.js
+++ src/components/Page/Splash/SplashScreen.js
@@ -9,23 +9,29 @@ const SplashScreen = () => {
 
   return (
       <>
-        <div className = "splash_background"> {/*-- background --*/}
+        {/*-- Start of background --*/}
+        <div className = "splash_background">
           <div className = "splash_overlay">
             <div className = "splash_content">
-              <h1 className = "splash_title"><span>{'{'}</span>{'Algorithm Visualizer'}<span>{'}'}</span>
+              <h1 className = "splash_title">
+                <span>{'{'}</span>{'Algorithm Visualizer'}<span>{'}'}</span>
               </h1>
               <h2 className = "splash_subheading">
                 <span>CU Boulder</span>
               </h2>
               <ul className = "splash_list">
-                <li><a href = "/about" className = "splash_link"><span>{'About'}</span></a></li>
-                <li><a href = "/contactUs" className = "splash_link"><span>{'Contact'}</span></a></li>
-                <li><a href = "/home" className = "splash_link"><span>{'Home'}</span></a></li>
+                <li><a href = "/about"
+                       className = "splash_link"><span>{'About'}</span></a></li>
+                <li><a href = "/contactUs"
+                       className = "splash_link"><span>{'Contact'}</span></a>
+                </li>
+                <li><a href = "/home"
+                       className = "splash_link"><span>{'Home'}</span></a></li>
               </ul>
             </div>
           </div>
         </div>
-        {/*-- background --*/}
+        {/*-- End of background --*/}
       </>
   );
 };
