d1f78976faa10f47dea95224197e170c24609e18
https-sam <osgo1115@icloud.com>
2022-07-12 07:50:08 -0600

    toggle btn fixed

--- src/components/Page/partial/Navbar/ThemeToggle.js
+++ src/components/Page/partial/Navbar/ThemeToggle.js
@@ -19,10 +19,10 @@ const ThemeToggle = () => {
     <div className={`${night ? 'bg-gradient-to-r from-gray-900 to-gray-800' : "bg-gradient-to-r from-gray-300 to-white"} w-[3.5em] h-[36px] bg-white rounded-full relative border-[2px] border-gray-400`} 
     onClick={() => { 
       setNightMode(!night)
-      localStorage.setItem("dark", !night);
+      localStorage.setItem("dark", JSON.stringify(!night));
     }}>
       <div className={`transition-all duration-300 ease-in-out absolute w-[28px] h-[28px] bg-gray-100 shadow-sm ${night ? "top-[.1em] translate-x-[1.75em] shadow-black" : " shadow-gray-400 top-[.1em] translate-x-[.1em]"} rounded-full`}>
-        <div className={`transition-all duration-300 ease-in-out absolute w-[26px] h-[26px] bg-gray-200 top-[3%] left-[7%] rounded-full flex items-center justify-center`}>
+        <div className={`transition-all duration-300 ease-in-out absolute w-[26px] h-[26px] bg-gray-200 top-[3%] left-[1%] rounded-full flex items-center justify-center`}>
           {night ? <Moon className={`text-gray-800 scale-[.8]`}/> : <Sun className={`text-gray-500 scale-[.8]`}/>}
         </div>
       </div>
