31cc1e6c82e939616a100102aac6c7ef1f107fad
https-sam <osgo1115@icloud.com>
2022-07-13 23:40:39 -0600

    minor bugs resolved

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -31,7 +31,7 @@ class Canvas extends Component {
         <div className="flex items-end">
           {generatedArray.map((element) => {
             return (
-              <div id={`${element}`} font-size={FONT_SIZE} data={`${(BAR_WIDTH > 25 && element > FONT_SIZE+16 && showHeight) ? element : ''}`} length="40px" key={Math.random()} style={{"--bar-font-size" :FONT_SIZE+"px", height: element+'px', width: BAR_WIDTH+'px', backgroundColor: this.props.barColor}} 
+              <div id={`${element}`} fontSize={FONT_SIZE} data={`${(BAR_WIDTH > 25 && element > FONT_SIZE+16 && showHeight) ? element : ''}`} length="40px" key={Math.random()} style={{"--bar-font-size" :FONT_SIZE+"px", height: element+'px', width: BAR_WIDTH+'px', backgroundColor: this.props.barColor}} 
               className={`array-bars m-[1px] rounded-t-md relative ${(defaultBarColor !== PINK && defaultBarColor !== PURPLE && defaultBarColor !== 'black') ? "after:text-gray-700" : "after:text-white"}`}>
                 {/* {(BAR_WIDTH > 38 && element > FONT_SIZE+16) && <p className={`text-center font-bold text-gray-800`} style={{fontSize: FONT_SIZE+"px"}}>{element}</p>}   */}
               </div>
